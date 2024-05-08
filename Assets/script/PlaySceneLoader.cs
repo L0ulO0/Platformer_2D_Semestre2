2b0e24b59adf45e710bfda83d3105a9beb23f0c5
L0ulO0 <celyagiv@gmail.com>
2024-05-08 10:45:24 +0200

    option_menu

--- Assets/script/PlaySceneLoader.cs
+++ Assets/script/PlaySceneLoader.cs
@@ -5,10 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class bouton_Start : MonoBehaviour
 {
+
+    [SerializeField] GameObject MainCanvas;
+    [SerializeField] GameObject OptionCanvas;
+    [SerializeField] GameObject Pancarte;
     public void PlayButton()
     {
 
         SceneManager.LoadScene("SCN_Niveau1");
         Debug.Log("load");
+    }
+
+    public void Option()
+    {
+        MainCanvas.SetActive(false);
+        Pancarte.SetActive(false);
+        OptionCanvas.SetActive(true);
+    }
+
+    public void Back()
+    {
+        MainCanvas.SetActive(true);
+        Pancarte.SetActive(true);
+        OptionCanvas.SetActive(false);
     }
 }
