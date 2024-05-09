7ae740e9fe7b707ac64a08eadd8729b866e8c0ef
L0ulO0 <celyagiv@gmail.com>
2024-05-09 14:31:30 +0200

    dialogue_code_reparation

--- Assets/script/Dialogue_Manager.cs
+++ Assets/script/Dialogue_Manager.cs
@@ -7,8 +7,8 @@ using TMPro;
 public class Dialogue_Manager : MonoBehaviour
 {
 
-    public TextMeshPro nametext;
-    public TextMeshPro dialoguetext;
+    public TextMeshProUGUI nametext;
+    public TextMeshProUGUI dialoguetext;
 
     public static Dialogue_Manager Instance;
     private void Awake()
