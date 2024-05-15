6fc566e7612930cbcf225e9d2c9e1a6e45f3f1bf
L0ulO0 <162572752+L0ulO0@users.noreply.github.com>
2024-05-15 14:07:40 +0200

    test_checkpoint

--- Assets/script/SpawnPlayer.cs
+++ Assets/script/SpawnPlayer.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour
+
+
+    
 {
+
+    [SerializeField] checkpoint PlayerSpawn;
     private void Awake()
     {
+        // checkpoint.PlayerSpawn.position = transform.position;
         GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
     }
 
